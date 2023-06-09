0dadf2b235e7e51d9fc887974ad3014b8e040ba2
Leo Jimenez <angel.jimenez2@g.austincc.edu>
2023-06-08 19:37:53 -0500

    Computer restart, checkpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import Header from "./components/HeaderComponents/Header";
 import LandingMain from "./components/LandingComponents/LandingMain";
+import InitButton from "./components/AlwaysHatedLamePorfolios/InitButton";
 import "./App.css";
 
 function App() {
   return (
     <div className='app'>
-    <div>
-    <Header style='header' />
-    </div>
-    
-      
+      <div>
+        <Header style='header' />
+      </div>
+
+      {/* here goes button component*/}
+
       <div className='intro'>
         <LandingMain></LandingMain>
       </div>
---
+++ src/components/AlwaysHatedLamePorfolios/InitButton.tsx
@@ -0,0 +1,25 @@
+
+
+const style: Array<string> = []
+
+
+
+
+
+const InitButton = () => {
+  const [displaying, setDisplaying] = useState([])
+  const handleOnClick = () = {
+
+  }
+  
+
+    return(
+      <button>Scroll not working ? :( </button>
+    )
+    
+  
+    
+}
+
+export default InitButton
+
