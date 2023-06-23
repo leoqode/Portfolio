39e3c916954ffa57dc366b24dd83cd2dfb7f1350
Leo Jimenez <angel.jimenez2@g.austincc.edu>
2023-06-23 05:57:58 -0500

    Stars added, matched LI and GIT blocks to rings

--- src/components/AlwaysHatedLamePorfolios/BlackHoleLetterAnimation.tsx
+++ src/components/AlwaysHatedLamePorfolios/BlackHoleLetterAnimation.tsx
@@ -1,11 +1,93 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import "./BlackHoleLetterAnimation.css";
 
 const BlackHoleComp = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const [spaceBackground, setSpaceBackground] = useState(true);
+  
+  
+
+
+
+
+
+
+//STARS CLASS ---------------------->
+
+
+  
+  class Star {
+    public positionX: number;
+    public positionY: number;
+    public positionZ: number;
+
+    public star: THREE.Mesh;
+    private closeNode: THREE.Object3D;
+    private shape: THREE.SphereGeometry;
+    private material: THREE.MeshBasicMaterial;
+    private linker: THREE.Line;
+    private destinationX: number;
+    private destinationY: number;
+    private destinationZ: number;
+
+    constructor() {
+      
+      this.positionX = Math.floor(Math.random() * 50 - 25)
+      this.positionY = Math.floor(Math.random() * 50 - 25)
+      this.positionZ = Math.floor(Math.random() * 50 - 25)
+      this.shape = new THREE.SphereGeometry(0.1, 32, 32);
+      this.material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+      this.closeNode = new THREE.Object3D();
+      this.linker = new THREE.Line();
+      this.destinationX = Math.floor(Math.random() * 50 - 25)
+      this.destinationY = Math.floor(Math.random() * 50 - 25)
+      this.destinationZ = Math.floor(Math.random() * 50 - 25)
+      this.star = new THREE.Mesh(this.shape, this.material);
+    }
+
+
+
+    update(): void {
+      
+
+
+      if (!this.isInBounds()) {
+        this.createRandomPosition();
+      }
+    }
+
+    private isInBounds(): boolean {
+      if (
+        this.positionX > 50 ||this.positionX < -50  ||
+        this.positionY > 50 ||this.positionY < -50  ||
+        this.positionZ > 50 ||this.positionZ < -50 
+      ) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+    private createRandomPosition() {
+      let x = Math.random() * window.innerHeight;
+      let y = Math.random() * window.innerWidth;
+      let z = Math.random() * Math.sqrt(window.innerWidth);
+
+      this.positionX = x
+      this.positionY = y
+      this.positionZ = z
+    }
+  }
+
+
+
+
+  
+
+
   useEffect(() => {
+    let stars: Array<Star> = [];
     if (canvasRef.current) {
       const sizes = {
         width: window.innerWidth,
@@ -19,21 +101,23 @@ const BlackHoleComp = () => {
         roughness: 0.5,
         metalness: 0.1,
       });
-      
+
       const mesh = new THREE.Mesh(sphere, sphereMaterial);
       scene.add(mesh);
 
-      const linkedInTexture = new THREE.TextureLoader().load('./src/assets/linkedInOrbit.png')
+      const linkedInTexture = new THREE.TextureLoader().load(
+        "./src/assets/linkedInOrbit.png"
+      );
       const linkedIn = new THREE.BoxGeometry(1, 1, 1);
       const materialLinkedIn = new THREE.MeshBasicMaterial({
         map: linkedInTexture,
       });
       const linkedInCube = new THREE.Mesh(linkedIn, materialLinkedIn);
-      linkedInCube.position.set(6, 6, 0);
       scene.add(linkedInCube);
 
-
-      const githubTexture = new THREE.TextureLoader().load('./src/assets/githubOrbit.png')
+      const githubTexture = new THREE.TextureLoader().load(
+        "./src/assets/githubOrbit.png"
+      );
       const githubBox = new THREE.BoxGeometry(1, 1, 1);
       const githubMaterial = new THREE.MeshBasicMaterial({
         map: githubTexture,
@@ -44,7 +128,6 @@ const BlackHoleComp = () => {
       const directionalLight = new THREE.DirectionalLight(0xffffff, 1); // Set light color to white (0xffffff) and intensity to 1
       directionalLight.position.set(0, 1, 0); // Set position to shine from above (0, 1, 0)
       scene.add(directionalLight);
-      
 
       const camera = new THREE.PerspectiveCamera(
         20,
@@ -85,14 +168,16 @@ const BlackHoleComp = () => {
       controls.rotateSpeed = 5;
 
       //grid helper---------------------------
-    {/*
+      {/*
+        
       const size = 40;
 
       const divisions = 40;
 
       const gridHelper = new THREE.GridHelper(size, divisions);
       scene.add(gridHelper);
-    */}
+    
+      */}
       //grid helper---------------------------
 
       window.addEventListener("resize", () => {
@@ -106,25 +191,46 @@ const BlackHoleComp = () => {
       });
       let t: number = 0;
       let t2: number = 0;
+      let starIteration: number = 0;
+      if (spaceBackground) {
+        for (let i = 0; i < 100; i++) {
+          let star = new Star();
+          star.star.position.setX(star.positionX)
+          star.star.position.setY(star.positionY)
+          star.star.position.setZ(star.positionZ)
+          stars.push(star);
+          scene.add(stars[i].star);
+        }
+        const starAnimation = () => {
+          window.requestAnimationFrame(starAnimation)
+
+          starIteration += 0.1
+
+
+          
+
+        };
+      }
       const loop = () => {
         window.requestAnimationFrame(loop);
-        
-        t += .02;
-        t2 += .026;
 
-        linkedInCube.rotation.y += 0.0;
-        githubMesh.rotation.x += 0.0;
+        t += 0.01;
+        t2 -= 0.019;
 
-        meshRing.rotation.y -= 0.02;
-        meshRing2.rotation.z -= 0.026;
 
+
+
+        linkedInCube.rotation.y += 0.01;
+        githubMesh.rotation.y += 0.01;
+
+        meshRing.rotation.y -= 0.01;
+        meshRing2.rotation.y += 0.019;
 
         githubMesh.position.x = 10 * Math.cos(t) + 0;
         githubMesh.position.z = 10 * Math.sin(t) + 0;
 
-
-        linkedInCube.position.y = 5 * Math.sin(t) + 0;
-        linkedInCube.position.x = 5 * Math.cos(t) + 0;
+        linkedInCube.position.x = 5 * Math.cos(t2) + 0;
+        linkedInCube.position.z = 5 * Math.sin(t2) + 0;
 
         renderer.render(scene, camera);
       };
